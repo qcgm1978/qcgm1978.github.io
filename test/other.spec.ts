28a8063eb29cc3c6b90e7381a03d56f278ab7b3b
zhanghl-k <zhanghl-k@glodon.com>
2018-04-25 18:34:19 +0800

    windows

--- test/other.spec.ts
+++ test/other.spec.ts
@@ -1,3 +1,4 @@
+import * as math from 'mathjs'
 describe(`https://en.wikipedia.org/wiki/SI_base_unit`, () => {
     it(`Seven SI base units`, () => {
         const SI = [{ "Name": "metre", "Symbol": "m", "Measure": "length", "DimensionSymbol": "L" }, { "Name": "kilogram", "Symbol": "kg", "Measure": "mass", "DimensionSymbol": "M" }, { "Name": "second", "Symbol": "s", "Measure": "time", "DimensionSymbol": "T" }, { "Name": "ampere", "Symbol": "A", "Measure": "electric current", "DimensionSymbol": "I" }, { "Name": "kelvin", "Symbol": "K", "Measure": "thermodynamic temperature", "DimensionSymbol": "Θ" }, { "Name": "mole", "Symbol": "mol", "Measure": "amount of substance", "DimensionSymbol": "N" }, { "Name": "candela", "Symbol": "cd", "Measure": "luminous intensity", "DimensionSymbol": "J" }];
