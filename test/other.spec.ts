6a617c91efea30ed511dd2fc68379e42e4bbcbc4
qcgm1978 <zhanghl@strongunion.com.cn>
2018-04-30 16:49:58 +0800

    octal

--- test/other.spec.ts
+++ test/other.spec.ts
@@ -1,4 +1,52 @@
 import * as math from 'mathjs'
+it(`https://en.wikipedia.org/wiki/Bernoulli_process`, () => {
+    let num = parseInt('101010', 2);
+    expect(num).toBe(42)
+    num = parseInt('10011011', 2)
+    expect(num).toBe(155)
+    num = Number(0o10011011.toString())
+    expect(num).toBe(2101769)
+});
+it(`https://stackoverflow.com/questions/30314603/insert-english-characters-get-greek-characters-back-javascript`, () => {
+    expect(toGreek('abc')).toEqual("αβψ")
+    function toGreek(chVal) {
+        var myObject;
+        myObject = {
+            "a": "α",
+            "b": "β",
+            "c": "ψ",
+            "d": "δ",
+            "e": "ε",
+            "f": "φ",
+            "g": "γ",
+            "h": "η",
+            "i": "ι",
+            "j": "ξ",
+            "k": "κ",
+            "l": "λ",
+            "m": "μ",
+            "n": "ν",
+            "o": "ο",
+            "p": "π",
+            "q": ";",
+            "r": "ρ",
+            "s": "σ",
+            "t": "τ",
+            "u": "θ",
+            "v": "ω",
+            "w": "ς",
+            "x": "χ",
+            "y": "υ",
+            "z": "ζ",
+            ";": ""
+        };
+        // for (const item of chVal) {
+        //     chVal = chVal.replace(item, myObject[item])
+        // }
+        return chVal.replace(/\w/g, item => myObject[item]);
+        // return chVal.map(a => myObject[a] || a).join('');
+    }
+});
 describe(`https://en.wikipedia.org/wiki/SI_base_unit`, () => {
     it(`Seven SI base units`, () => {
         const SI = [{ "Name": "metre", "Symbol": "m", "Measure": "length", "DimensionSymbol": "L" }, { "Name": "kilogram", "Symbol": "kg", "Measure": "mass", "DimensionSymbol": "M" }, { "Name": "second", "Symbol": "s", "Measure": "time", "DimensionSymbol": "T" }, { "Name": "ampere", "Symbol": "A", "Measure": "electric current", "DimensionSymbol": "I" }, { "Name": "kelvin", "Symbol": "K", "Measure": "thermodynamic temperature", "DimensionSymbol": "Θ" }, { "Name": "mole", "Symbol": "mol", "Measure": "amount of substance", "DimensionSymbol": "N" }, { "Name": "candela", "Symbol": "cd", "Measure": "luminous intensity", "DimensionSymbol": "J" }];
