f186ec4b3481ff56230097e74a09168221bf95ed
qcgm1978 <zhanghl@strongunion.com.cn>
2018-05-06 13:29:39 +0800

    js

--- module/demo.ts
+++ module/demo.ts
@@ -1,6 +1,9 @@
+// import { define } from 'requirejs'
 import data from '../data/go'
-import * as $ from 'jquery';
+// import * as $ from 'jquery';
+if (navigator.platform) {
 
-$('[data-wgo]').attr('data-wgo', data)
+    $('[data-wgo]').attr('data-wgo', data)
+}
 debugger;
 // document.getElementById('container').setAttribute('data-wgo', data)
